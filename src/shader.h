58ffffccb6ab846f2d7f43be78b08cc9931c99af
xfry <fry.andrade@gmail.com>
2024-11-14 06:50:48 -0500

    added Shader class for loading vertex and fragment shaders

---
+++ src/shader.h
@@ -0,0 +1,21 @@
+#ifndef __Shader_H__
+#define __Shader_H__
+
+#include <string>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+class Shader {
+
+public:
+    Shader( const char* vertexPath, const char* fragmentPath );
+    void use();
+    void setMat4( const std::string* name, const glm::mat4& value );
+private:
+    unsigned int Id;
+    std::string loadShader( const char* path );
+    void checkCompileErrors( GLint shader, std::string type );
+
+};
+
+#endif
